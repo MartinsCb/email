5fa9f4c84d1681896b991bc313f72633d9f1c2cf
marti <andremartins.a@outlook.com>
2024-09-08 11:10:32 -0300

    configurando ambiente de email

--- send-mail/src/test/java/send/mail/AppTest.java
+++ send-mail/src/test/java/send/mail/AppTest.java
@@ -9,11 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Properties;
 
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 @ExtendWith(OutputCaptureExtension.class)
 public class AppTest {
+	
+	private String userName = "andredeveloperjava@gmail.com";
+	private String senha = "231928an";
+	
 	@Test
 	public void testEmail() {
-		
+		try {
 		//configurando as propriedades
 		
 		Properties properties = new Properties();
@@ -24,7 +32,15 @@ public class AppTest {
 		properties.put("mail.smtp.socketFactory.port","465");//porta a ser conectada pelo socket
 		properties.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");//classe spcket de conexão
 		
-		
+		Session session = Session.getInstance(properties, new Authenticator() {
+			protected PasswordAuthentication getPasswordAutenthication() {
+				return new PasswordAuthentication(userName, senha);
+			}
+		});
+		System.out.println(senha);
+		}catch (Exception e ) {
+			e.printStackTrace();
+		}
 	}
 
    
